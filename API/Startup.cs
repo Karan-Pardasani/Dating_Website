7187e6b7f8be67bf23262ce18f966cd875e2c144
Karan-Pardasani <karanpardasani466@gmail.com>
2022-09-27 01:54:51 -0400

    Basic Authentication (Store Passwords)

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +15,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 namespace API
 {
     public class Startup
@@ -26,17 +31,34 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddScoped<ITokenService, TokenService>();
 
-            services.AddDbContext<Data.DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            // services.AddDbContext<Data.DataContext>(options =>
+            // {
+            //     options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            // });
+
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+
+            // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+            //     options =>
+            //     {
+            //         options.TokenValidationParameters = new TokenValidationParameters
+            //         {
+            //             ValidateIssuerSigningKey = true,
+            //             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
+            //             ValidateIssuer = false,
+            //             ValidateAudience = false,
+            //         };
+            //     }
+            // );
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +76,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
