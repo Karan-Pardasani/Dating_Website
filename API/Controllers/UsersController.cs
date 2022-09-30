41788d7d373e21f59dae373ee8f3374b3d0ea944
Karan-Pardasani <karanpardasani466@gmail.com>
2022-09-30 00:25:44 -0400

    Added Loading

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using API.DTOs;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -39,6 +40,18 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
 
     }
 }
